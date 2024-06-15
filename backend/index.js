039160c3b85ad170bfe27577797544da3d51c92c
Syed Ahsan <syedahsannoori@gmail.com>
2024-06-15 12:41:58 +0500

    mongoDB connected

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,18 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 app.listen(3000, () => {
-    console.log('Server is running on port 3000');  
-})
+    console.log('Server is running on port 3000');
+});
+
+mongoose.connect(process.env.MONGO)
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch((error) => {
+        console.log('Not Connected');
+        console.log({error});
+    });
