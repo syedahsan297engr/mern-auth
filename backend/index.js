69e22312e723108812ca16350543bccb9c0fa4fa
Syed Ahsan <syedahsannoori@gmail.com>
2024-06-15 15:07:12 +0500

    create userModel

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,7 @@ app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
 
+//I have used the previous uri in the .env file
 mongoose.connect(process.env.MONGO)
     .then(() => {
         console.log('Connected to MongoDB');
