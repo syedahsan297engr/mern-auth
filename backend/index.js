46f0800afa8ee16d3f1de2a9d13860978c57d260
Syed Ahsan <syedahsannoori@gmail.com>
2024-07-16 15:29:16 +0500

    add pre production changes

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
+import path from 'path';
 dotenv.config();
 
 const app = express();
@@ -24,6 +25,13 @@ mongoose.connect(process.env.MONGO)
         console.log('Not Connected');
         console.log({error});
     });
+
+const __dirname = path.resolve(); //for server where we upload this
+app.use(express.static(path.join(__dirname, '/client/dist')));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client','dist','index.html'));
+});
+
 
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
