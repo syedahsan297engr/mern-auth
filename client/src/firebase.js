611b427d93feef952141d0129d6d3eca4f5f1d1c
Syed Ahsan <syedahsannoori@gmail.com>
2024-12-21 16:02:52 +0500

    lil update

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,16 +1,18 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import dotenv from "dotenv";
+dotenv.config();
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCW0kxb-i5ljPzzViHY6ZQBQr8O-bGTYSQ", //import.env.VITE_FIREBASE_API_KEY from .env file
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "mern-auth-f72ff.firebaseapp.com",
   projectId: "mern-auth-f72ff",
   storageBucket: "mern-auth-f72ff.appspot.com",
   messagingSenderId: "387037736209",
-  appId: "1:387037736209:web:0dd6db602cfc30f4cf30b0"
+  appId: "1:387037736209:web:0dd6db602cfc30f4cf30b0",
 };
 
 // Initialize Firebase
